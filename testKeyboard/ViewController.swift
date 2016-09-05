958b342eea37432221115221f24e0730a17ba3e0
Nicolas Martin <nicolas.martin.0986@gmail.com>
2016-09-05 19:22:06 -0400

    Initial Commit

---
+++ testKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testKeyboard
+//
+//  Created by Nicolas Martin on 2016-09-05.
+//  Copyright © 2016 Nicolas Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
